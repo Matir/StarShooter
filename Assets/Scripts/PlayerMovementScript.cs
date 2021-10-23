bdd49568751141bf80227ddf7e8d37f0db3f3b2d
David Tomaschik <david@systemoverlord.com>
2021-10-23 16:13:32 -0700

    Add Fighter/shots

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -7,8 +7,13 @@ using UnityEngine;
 public class PlayerMovementScript : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float minFireDelay = 1.0f;
+    public GameObject projectile;
+
     private float threshold = 0.01f;
     private Rigidbody2D rigidBody;
+    private bool firing = false;
+    private float lastFireTime = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,11 @@ public class PlayerMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        firing = Input.GetButton("Fire1");
+        if (firing && (Time.time > lastFireTime + minFireDelay)) {
+            lastFireTime = Time.time;
+            FireCannon();
+        }
     }
 
     // FixedUpdate is called on a Fixed interval
@@ -33,5 +43,12 @@ public class PlayerMovementScript : MonoBehaviour
         }
         movement *= speed;
         rigidBody.velocity = new Vector2(movement, 0);
+    }
+
+    private void FireCannon() {
+        Debug.Log("Firing cannon!");
+        GameObject fired = Instantiate(projectile, transform.position, transform.rotation);
+        // Todo: per-ammo launch type?
+        fired.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(0, 5, 0));
     }
 }
